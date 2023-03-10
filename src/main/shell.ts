1a19a27130b3d9eee2c9ee570856e544ea29720f
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2023-03-10 14:20:08 +0400

    fix: path

--- src/main/shell.ts
+++ src/main/shell.ts
@@ -1,6 +1,8 @@
 import util from 'node:util';
 import child_process from 'node:child_process';
 
+import { log } from './logger';
+
 const exec = util.promisify(child_process.exec);
 
 const isNotNodeModulesPath = (path: string): boolean => !path.includes('node_modules');
@@ -14,6 +16,9 @@ export const getNgrokPath = async (): Promise<string[] | null> => {
 
     const paths = stdout.trim().split('\n').filter(isNotNodeModulesPath);
 
+    // log.silly(`stdout: ${stdout}`);
+    // log.silly(`stderr: ${stderr}`);
+
     if (paths.length === 0) {
       return null;
     }
@@ -21,6 +26,12 @@ export const getNgrokPath = async (): Promise<string[] | null> => {
     return paths;
   } catch (error) {
     console.log('shell error', error);
+    console.log('shell error2', JSON.stringify(error));
+    log.silly(`shell error message: ${error}`);
+    log.silly(`shell error JSON: ${JSON.stringify(error)}`);
+    log.silly(`execpath: ${process.execPath}`);
+    log.silly(`env: ${JSON.stringify(process.env)}`);
+
     return null;
   }
 };
