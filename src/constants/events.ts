f7a0ef46c7ba8d936aef951a450a2824e85b3e41
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2023-03-10 11:46:02 +0400

    refactor: ipc event

--- src/constants/events.ts
+++ src/constants/events.ts
@@ -2,7 +2,7 @@ export const APP_EVENTS = {
   IPC: {
     LOADED: 'app.ipc.loaded',
     OPEN_FILE: 'app.ipc.open_file',
-    SHELL_FIND_NGROK: 'app.ipc.shell_find_ngrok',
+    SHELL_NGROK_PATH: 'app.ipc.shell_ngrok_path',
     START_NGROK_TUNNEL: 'app.ipc.start_ngrok_tunnel',
   },
 } as const;
