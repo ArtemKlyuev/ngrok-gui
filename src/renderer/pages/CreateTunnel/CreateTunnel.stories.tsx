5aefb382cc807f883ef0b8faa86e3fec88babdda
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2023-04-07 19:54:58 +0400

    feat(page): add `CreateTunnel` story

--- src/renderer/pages/CreateTunnel/CreateTunnel.stories.tsx
+++ src/renderer/pages/CreateTunnel/CreateTunnel.stories.tsx
@@ -1,26 +1,23 @@
-// Button.stories.ts|tsx
-
 import type { Meta, StoryObj } from '@storybook/react';
 import { withRouter } from 'storybook-addon-react-router-v6';
 
+import { withExposedAPI } from '../../../../.storybook/decorators';
+
 import { CreateTunnel as Page } from './CreateTunnel';
+import { action } from '@storybook/addon-actions';
 
 const meta: Meta<typeof Page> = {
-  /* 👇 The title prop is optional.
-   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-   * to learn how to generate automatic titles
-   */
   title: 'Pages/Create Tunnel',
-  decorators: [withRouter],
+  decorators: [withRouter, withExposedAPI],
   component: Page,
+  parameters: { api: { startTunnel: action('startTunnel triggered') } },
 };
 
 export default meta;
 type Story = StoryObj<typeof Page>;
 
-/*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
- * See https://storybook.js.org/docs/react/api/csf
- * to learn how to use render functions.
- */
-export const CreateTunnel: Story = {};
+export const WithPredefinedPath: Story = {
+  parameters: { ngrokPath: ['bin/ngrok', 'user/var/ngrok'] },
+};
+
+export const WithoutPredefinedPath: Story = {};
