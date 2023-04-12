b8b9c6d716cbda0c0bf94cc906fccf4f805a0328
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2023-04-12 15:56:14 +0400

    refactor: `storybook` decorators

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -5,16 +5,10 @@ import { RENDERER } from '../src/constants/common';
 type Decorator = Required<Meta>['decorators'][0];
 
 export const withExposedAPI: Decorator = (Story, context) => {
-  const data = { [RENDERER.EXPOSED_API.KEYS.NGROK_PATH]: null };
-  const api = { startTunnel: null };
-
-  if (context.parameters.ngrokPath) {
-    data[RENDERER.EXPOSED_API.KEYS.NGROK_PATH] = context.parameters.ngrokPath;
-  }
-
-  if (context.parameters.api?.startTunnel) {
-    api.startTunnel = context.parameters.api?.startTunnel;
-  }
+  const data = {
+    [RENDERER.EXPOSED_API.KEYS.NGROK_PATH]: context.parameters.ngrokPath ?? null,
+  };
+  const api = { ...context.parameters.api };
 
   // @ts-expect-error
   window[RENDERER.EXPOSED_API.NAME] = { api, preloaded: new Map(Object.entries(data)) };
