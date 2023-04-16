da7fd59e064e4438756b5dd1c7b2c10eb1c40679
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2023-04-16 14:43:53 +0400

    feat: add loading state to button

--- src/renderer/components/Button.tsx
+++ src/renderer/components/Button.tsx
@@ -4,6 +4,7 @@ import cn from 'classnames';
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant: 'primary' | 'error';
   size: 'small' | 'medium';
+  loading?: boolean | undefined;
 }
 
 const sizeToClassName = {
@@ -16,11 +17,22 @@ const variantToClassName = {
   error: 'btn-error',
 };
 
-export const Button: FC<Props> = ({ variant, size, type = 'button', ...props }) => {
+export const Button: FC<Props> = ({
+  variant,
+  size,
+  type = 'button',
+  loading = false,
+  ...props
+}) => {
   return (
     <button
       type={type}
-      className={cn('btn', sizeToClassName[size], variantToClassName[variant])}
+      className={cn(
+        'btn',
+        sizeToClassName[size],
+        variantToClassName[variant],
+        loading && 'loading',
+      )}
       {...props}
     />
   );
