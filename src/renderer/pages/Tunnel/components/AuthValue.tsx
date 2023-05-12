959a6346e462d7175bde21e6be7ad0371b1f1377
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2023-05-12 17:50:25 +0400

    refactor: `utils`

--- src/renderer/pages/Tunnel/components/AuthValue.tsx
+++ src/renderer/pages/Tunnel/components/AuthValue.tsx
@@ -1,42 +1,10 @@
 import { CopyIcon } from '../../../icons';
+import { copyTextToClipboard } from '../../../utils';
 
 interface Props {
   value: string;
   hideValue?: boolean | undefined;
 }
-
-const fallbackCopyTextToClipboard = (text: string): void => {
-  const textArea = document.createElement('textarea');
-  textArea.style.position = 'fixed';
-  textArea.style.top = '0';
-  textArea.style.left = '0';
-  textArea.style.visibility = 'none';
-
-  textArea.value = text;
-
-  document.body.appendChild(textArea);
-  textArea.focus();
-  textArea.select();
-
-  try {
-    document.execCommand('copy');
-  } catch (e) {
-    console.error('fallback copy to clipboard fail', e);
-  }
-
-  document.body.removeChild(textArea);
-};
-
-export const copyTextToClipboard = async (text: string): Promise<void> => {
-  /**
-   * @see https://stackoverflow.com/questions/51805395/navigator-clipboard-is-undefined
-   */
-  try {
-    await navigator.clipboard.writeText(text);
-  } catch (e) {
-    fallbackCopyTextToClipboard(text);
-  }
-};
 
 export const AuthValue = ({ value, hideValue = false }: Props) => {
   const type = hideValue ? 'password' : 'text';
